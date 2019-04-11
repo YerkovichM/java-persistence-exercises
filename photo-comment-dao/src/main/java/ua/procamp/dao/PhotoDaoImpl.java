026743a3cf4b76329386af39447b2983abc3e8ea
Marko Yerkovich <yerkovich.m@gmail.com>
2019-04-11 20:06:54 +0300

    Clean up code a little photo-comment-dao

--- photo-comment-dao/src/main/java/ua/procamp/dao/PhotoDaoImpl.java
+++ photo-comment-dao/src/main/java/ua/procamp/dao/PhotoDaoImpl.java
@@ -1,14 +1,16 @@
 package ua.procamp.dao;
 
+import org.hibernate.jpa.QueryHints;
 import ua.procamp.model.Photo;
 import ua.procamp.model.PhotoComment;
 import ua.procamp.util.EntityManagerUtil;
 
-import javax.persistence.Entity;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 /**
  * Please note that you should not use auto-commit mode for your implementation.
@@ -22,27 +24,23 @@ public class PhotoDaoImpl implements PhotoDao {
 
     @Override
     public void save(Photo photo) {
-        EntityManagerUtil inTrans = new EntityManagerUtil(entityManagerFactory);
-        inTrans.performWithinTx(em -> em.persist(photo));
+        doInTrans(em -> em.persist(photo));
     }
 
     @Override
     public Photo findById(long id) {
-        EntityManagerUtil inTrans = new EntityManagerUtil(entityManagerFactory);
-        return inTrans.performReturningWithinTx(em -> em.find(Photo.class, id));
+        return doInTransWithReturnReadOnly(em -> em.find(Photo.class, id));
     }
 
     @Override
     public List<Photo> findAll() {
-        EntityManagerUtil inTrans = new EntityManagerUtil(entityManagerFactory);
-        return inTrans.performReturningWithinTx(em -> em.createQuery("select p from Photo p", Photo.class)
+        return doInTransWithReturnReadOnly(em -> em.createQuery("select p from Photo p", Photo.class)
                     .getResultList());
     }
 
     @Override
     public void remove(Photo photo) {
-        EntityManagerUtil inTrans = new EntityManagerUtil(entityManagerFactory);
-        inTrans.performWithinTx(em -> {
+        doInTrans(em -> {
             Photo foundPhoto = em.merge(photo);
             em.remove(foundPhoto);
         });
@@ -51,13 +49,49 @@ public class PhotoDaoImpl implements PhotoDao {
     @Override
     public void addComment(long photoId, String comment) {
         EntityManagerUtil inTrans = new EntityManagerUtil(entityManagerFactory);
-        inTrans.performWithinTx(em -> {
-            Photo photo = em.find(Photo.class, photoId);
+        doInTrans(em -> {
+            Photo photo = em.getReference(Photo.class, photoId);
             PhotoComment photoComment = new PhotoComment();
             photoComment.setPhoto(photo);
             photoComment.setText(comment);
             photoComment.setCreatedOn(LocalDateTime.now());
             em.persist(photoComment);
+        });
+    }
+
+    private void doInTrans(Consumer<EntityManager> operation){
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        try {
+            operation.accept(entityManager);
+            entityManager.getTransaction().commit();
+        }catch (Exception e){
+            entityManager.getTransaction().rollback();
+            throw e;
+        }finally {
+            entityManager.close();
+        }
+    }
+
+    private <T> T doInTransWithReturn(Function<EntityManager, T> operation){
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+            try {
+                T result = operation.apply(entityManager);
+                entityManager.getTransaction().commit();
+                return result;
+            }catch (Exception e){
+                entityManager.getTransaction().rollback();
+                throw e;
+            }finally {
+                entityManager.close();
+            }
+    }
+
+    private <T> T doInTransWithReturnReadOnly(Function<EntityManager, T> operation){
+        return doInTransWithReturn(entityManager -> {
+           entityManager.setProperty(QueryHints.HINT_READONLY, true);
+           return operation.apply(entityManager);
         });
     }
 }
