5baf5323746849482270be0152208fad08bae10d
Marko Yerkovich <yerkovich.m@gmail.com>
2019-04-11 23:20:18 +0300

    Clean up code AccountDaoImpl.java Add Card class to persistence.xml

--- account-dao/src/main/java/ua/procamp/dao/AccountDaoImpl.java
+++ account-dao/src/main/java/ua/procamp/dao/AccountDaoImpl.java
@@ -1,12 +1,14 @@
 package ua.procamp.dao;
 
+import org.hibernate.jpa.QueryHints;
 import ua.procamp.exception.AccountDaoException;
 import ua.procamp.model.Account;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class AccountDaoImpl implements AccountDao {
     private EntityManagerFactory emf;
@@ -17,107 +19,79 @@ public class AccountDaoImpl implements AccountDao {
 
     @Override
     public void save(Account account) {
-        EntityManager entityManager = emf.createEntityManager();
-        entityManager.getTransaction().begin();
-        try {
-            entityManager.persist(account);
-            entityManager.getTransaction().commit();
-        }catch (Exception e){
-            entityManager.getTransaction().rollback();
-            throw new AccountDaoException("Exception during transaction", e);
-        }finally {
-            entityManager.close();
-        }
+        doInTrans(entityManager -> entityManager.persist(account));
     }
 
     @Override
     public Account findById(Long id) {
-        EntityManager entityManager = emf.createEntityManager();
-        entityManager.getTransaction().begin();
-        Account res = null;
-        try {
-             res = entityManager
-                     .createQuery("select a from Account a where id = :id", Account.class)
-                    .setParameter("id", id)
-                    .getSingleResult();
-             entityManager.getTransaction().commit();
-        }catch (Exception e){
-            entityManager.getTransaction().rollback();
-            throw new AccountDaoException("Exception during transaction", e);
-        }finally {
-            entityManager.close();
-        }
-        return res;
+        return doInTransWithReturnReadOnly(entityManager -> entityManager
+                .createQuery("select a from Account a where id = :id", Account.class)
+                .setParameter("id", id)
+                .getSingleResult());
     }
 
     @Override
     public Account findByEmail(String email) {
-        EntityManager entityManager = emf.createEntityManager();
-        entityManager.getTransaction().begin();
-        Account res = null;
-        try {
-            res = entityManager
-                    .createQuery("select a from Account a where email = :email", Account.class)
-                    .setParameter("email", email)
-                    .getSingleResult();
-            entityManager.getTransaction().commit();
-        }catch (Exception e){
-            entityManager.getTransaction().rollback();
-            throw new AccountDaoException("Exception during transaction", e);
-        }finally {
-            entityManager.close();
-        }
-        return res;
+        return doInTransWithReturnReadOnly(entityManager -> entityManager
+                .createQuery("select a from Account a where email = :email", Account.class)
+                .setParameter("email", email)
+                .getSingleResult());
     }
 
     @Override
     public List<Account> findAll() {
-        EntityManager entityManager = emf.createEntityManager();
-        entityManager.getTransaction().begin();
-        List<Account> res;
-        try {
-            res = entityManager
-                    .createQuery("select a from Account a", Account.class)
-                    .getResultList();
-            entityManager.getTransaction().commit();
-        }catch (Exception e){
-            entityManager.getTransaction().rollback();
-            throw new AccountDaoException("Exception during transaction", e);
-        }finally {
-            entityManager.close();
-        }
-        return res;
+        return doInTransWithReturnReadOnly(entityManager -> entityManager
+                .createQuery("select a from Account a", Account.class)
+                .getResultList());
     }
 
     @Override
     public void update(Account account) {
-        EntityManager entityManager = emf.createEntityManager();
-        entityManager.getTransaction().begin();
-        try {
-            entityManager.merge(account);
-            entityManager.getTransaction().commit();
-        }catch (Exception e){
-            entityManager.getTransaction().rollback();
-            throw new AccountDaoException("Exception during transaction", e);
-        }finally {
-            entityManager.close();
-        }
+        doInTrans(entityManager -> entityManager.merge(account));
     }
 
     @Override
     public void remove(Account account) {
+        doInTrans(entityManager -> {
+            Account merge = entityManager.merge(account);
+            entityManager.remove(merge);
+        });
+    }
+
+    private void doInTrans(Consumer<EntityManager> operation) {
         EntityManager entityManager = emf.createEntityManager();
         entityManager.getTransaction().begin();
         try {
-            Account merge = entityManager.merge(account);
-            entityManager.remove(merge);
+            operation.accept(entityManager);
             entityManager.getTransaction().commit();
-        }catch (Exception e){
+        } catch (Exception e) {
             entityManager.getTransaction().rollback();
             throw new AccountDaoException("Exception during transaction", e);
-        }finally {
+        } finally {
             entityManager.close();
         }
+    }
+
+    private <T> T doInTransWithReturn(Function<EntityManager, T> operation) {
+        EntityManager entityManager = emf.createEntityManager();
+        entityManager.getTransaction().begin();
+        try {
+            T result = operation.apply(entityManager);
+            entityManager.getTransaction().commit();
+            return result;
+        } catch (Exception e) {
+            entityManager.getTransaction().rollback();
+            throw new AccountDaoException("Exception during transaction", e);
+        } finally {
+            entityManager.close();
+        }
+    }
+
+    private <T> T doInTransWithReturnReadOnly(Function<EntityManager, T> operation) {
+        return doInTransWithReturn(entityManager -> {
+            entityManager.setProperty(QueryHints.HINT_READONLY, true);
+            return operation.apply(entityManager);
+        });
     }
 }
 
